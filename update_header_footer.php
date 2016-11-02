55c47f59bad03b76ca1500b01b58d9175d897951
Kyle Francis <guitarman_usa@yahoo.com>
2016-11-02 11:03:41 -0400

    Formatted pages to have header and footer tags necessary for updating via php script

---
+++ update_header_footer.php
@@ -0,0 +1,28 @@
+<?php
+  $files = array(
+    "co_bio.html",
+#    "contact.html",
+#    "history.html",
+#    "index.html",
+#    "whatisnrotc.html",
+#    "xo_bio.html"
+  );
+
+  foreach ($files as $file) {
+    echo "Found file: $file";
+    # Replace Header
+    $replaced_text = preg_replace("/<!-- Header -->.*<!-- End Header -->/ms",
+                                  file_get_contents("index_header.html"),
+                                  file_get_contents($file));
+    echo $replaced_text;
+    file_put_contents($file, $replaced_text);
+
+    # Replace Footer
+    $replaced_text = preg_replace("/<!-- Footer -->.*<!-- End Footer -->/ms",
+                                  file_get_contents("index_footer.html"),
+                                  file_get_contents($file));
+    echo $replaced_text;
+    file_put_contents($file, $replaced_text);
+  }
+
+?>
